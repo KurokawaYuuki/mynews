28635ee2bf18e7376a63ff6f21eb63dcad1bb71d
Kurokawa Yuuki <weereup@gmail.com>
2020-08-12 14:39:29 +0000

    update

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -8,6 +8,22 @@
         <title>MyCreate</title>
     </head>
     <body>
-        <h1>Myクリエート画面</h1>
+        <h1>MyCreate画面</h1>
     </body>
+    
+@extends('layouts.profile')
+
+@section('title' , '新規Create作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Create新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+
 </html>
+
