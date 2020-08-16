03b7fac93557a56ee42bb533376b51a883c647be
Kurokawa Yuuki <weereup@gmail.com>
2020-08-16 12:31:55 +0000

    PHP/Laravel 13

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -8,6 +8,47 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>Create新規作成</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-date">
+                    
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">名前</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="user-name" value="{{ old('user-name') }}">
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <lalel class="col-md-2">性別</lalel>
+                        <div class="col-md-10">
+                            <input type="radio" name="gender" value="male">男 {{ old('user-name') }}
+                            <input type="radio" name="gender" value="female">女 {{ old('user-name') }}
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="hobby" rows="5">{{ old('hobby') }}</textarea>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="15">{{ old('hobby') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
