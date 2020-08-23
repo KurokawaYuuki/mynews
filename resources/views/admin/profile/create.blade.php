dcba8a21b6f64f66e977e2a8677020af39c02e50
Kurokawa Yuuki <weereup@gmail.com>
2020-08-23 01:11:45 +0000

    課題の修正13

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -28,8 +28,8 @@
                     <div class="form-group row">
                         <lalel class="col-md-2">性別</lalel>
                         <div class="col-md-10">
-                            <input type="radio" name="gender" value="{{ old('gender') }}">男 
-                            <input type="radio" name="gender" value="{{ old('gender') }}">女 
+                            <input type="radio" name="gender" value="男" {{ (old('gender') =='男') ? "checked" : "" }}>男
+                            <input type="radio" name="gender" value="女" {{ (old('gender') =='女') ? "checked" : "" }}>女
                         </div>
                     </div>
                     
