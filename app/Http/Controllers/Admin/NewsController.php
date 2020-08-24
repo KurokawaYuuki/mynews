3139c70c694deaa41ca4d9116926ddb483f2863c
Kurokawa Yuuki <weereup@gmail.com>
2020-08-24 12:17:41 +0000

    PHP/Laravel 16 投稿したニュースを更新/削除しよう

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -43,4 +43,74 @@ class NewsController extends Controller
       // admin/news/createにリダイレクトする
       return redirect('admin/news/create');
   }  
-} 
+  
+  public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != "") {
+          $posts = News::where('titile', $cond_title)->get();
+      }else{
+          $posts = News::all();
+      }
+      return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+  
+  public function edit(Request $request)
+  {
+    $news = News::find($request->id);
+    if (empty($news)) {
+      abort(404);
+    }
+    return view('admin.news.edit',['news_form' => $news]);
+  }
+  
+  public function update(Request $request)
+  {
+    // Validationをかける
+    $this->validate($request,News::$rules);
+    // News Modelからデータを取得する
+    $news = News::find($request, News::rules);
+    // 送信されてきたフォームデータを格納する
+    $news_form = $request->all();
+    if (isset($news_form['image'])){
+      $path = $request->file('image')->store('public/image');
+      $news->image_path = basename($path);
+      unset($news_form['image']);
+    }elseif (isset($request->remove)) {
+      $news->image_path = null;
+      unset($new_form['remove']);
+    }
+    unset($new_form['_token']);
+    // 該当するデータを上書きして保存する
+    $news->fill($news_form)->save();  
+    
+    return redirect('admin/news/');
+    }
+    
+  public function delete(Request $request)
+  {
+    $news = News::find($request->id);
+    $news->delete();
+    return redirect('admin/news/');
+  
+  }
+  
+}
+  
+ 
+
+
+
+
+/*
+$this->validate($request, [
+            'title'  => 'required',
+            ‘body’ => 'required',
+         ])
+         
+ public static $rules = array(
+      "title" => "required",
+      "body" => "required",
+    );
+*/        
+         
--- routes/web.php
+++ routes/web.php
@@ -15,36 +15,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-});
-
 /*http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。*/
 Route::get('XXX','AAAController@bbb');
-
-Route::group(['prefix' => 'admin'],function() {
-    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
-});
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create'); 
-});
-
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create');
-});
- 
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
     Route::get('profile/edit', 'Admin\ProfileController@edit');
     Route::post('profile/edit', 'Admin\ProfileController@update');
+    Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/edit', 'Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update');
+    Route::get('news/delete', 'Admin\NewsController@delete');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
